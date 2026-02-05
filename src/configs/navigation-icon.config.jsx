48ffb19b5042bc3107f7844d6521566470042cdd
Klisman <klisoved28@gmail.com>
2026-02-05 11:13:44 -0600

    fix(sidebar): replace missing Lucide icons with safe alternatives from installed react-icons version

--- src/configs/navigation-icon.config.jsx
+++ src/configs/navigation-icon.config.jsx
@@ -6,22 +6,22 @@ import {
     LuShoppingCart,
     LuShoppingBag,
     LuHistory,
-    LuSettings,
-    LuMonitor,
+    LuCog,
+    LuTv,
     LuWarehouse,
-    LuPackagePlus,
+    LuDownload,
     LuArrowLeftRight,
-    LuClipboardList,
-    LuLayoutGrid,
+    LuClipboard,
+    LuGrid,
     LuPackage,
-    LuShapes,
-    LuListTree,
-    LuBadgeCheck,
+    LuBox,
+    LuList,
+    LuCheckCircle,
     LuUsers,
-    LuBuilding2,
-    LuUsersCog,
+    LuBuilding,
+    LuBriefcase,
     LuUserCog,
-    LuIdCard,
+    LuBadge,
     LuTruck,
     LuStickyNote
 } from 'react-icons/lu'
@@ -35,36 +35,36 @@ const navigationIcon = {
     salesOpeningNewIcon: (props) => <LuShoppingCart size={ICON_SIZE} {...props} />,
     salesOpeningIcon: (props) => <LuShoppingBag size={ICON_SIZE} {...props} />,
     salesReportIcon: (props) => <LuHistory size={ICON_SIZE} {...props} />,
-    configsIcon: (props) => <LuSettings size={ICON_SIZE} {...props} />,
-    cashiersIcon: (props) => <LuMonitor size={ICON_SIZE} {...props} />,
+    configsIcon: (props) => <LuCog size={ICON_SIZE} {...props} />,
+    cashiersIcon: (props) => <LuTv size={ICON_SIZE} {...props} />,
 
     // Almacen Group & Items
     supplierIcon: (props) => <LuWarehouse size={ICON_SIZE} {...props} />, // Almacen Group
     warehouseIcon: (props) => <LuWarehouse size={ICON_SIZE} {...props} />, // Bodegas
-    stockInIcon: (props) => <LuPackagePlus size={ICON_SIZE} {...props} />,
-    transfersIcon: (props) => <LuArrowLeftRight size={ICON_SIZE} {...props} />, // New key: Transferencias
+    stockInIcon: (props) => <LuDownload size={ICON_SIZE} {...props} />,
+    transfersIcon: (props) => <LuArrowLeftRight size={ICON_SIZE} {...props} />,
 
     // Compras
-    purchasesIcon: (props) => <LuClipboardList size={ICON_SIZE} {...props} />,
+    purchasesIcon: (props) => <LuClipboard size={ICON_SIZE} {...props} />,
 
     // Catalogue
-    catalogueIcon: (props) => <LuLayoutGrid size={ICON_SIZE} {...props} />,
+    catalogueIcon: (props) => <LuGrid size={ICON_SIZE} {...props} />,
     productsIcon: (props) => <LuPackage size={ICON_SIZE} {...props} />,
-    categoriesIcon: (props) => <LuShapes size={ICON_SIZE} {...props} />,
-    subcategoriesIcon: (props) => <LuListTree size={ICON_SIZE} {...props} />,
-    brandsIcon: (props) => <LuBadgeCheck size={ICON_SIZE} {...props} />,
+    categoriesIcon: (props) => <LuBox size={ICON_SIZE} {...props} />,
+    subcategoriesIcon: (props) => <LuList size={ICON_SIZE} {...props} />,
+    brandsIcon: (props) => <LuCheckCircle size={ICON_SIZE} {...props} />,
 
     // Clients
     groupsIcon: (props) => <LuUsers size={ICON_SIZE} {...props} />, // Client Group
     customersIcon: (props) => <LuUsers size={ICON_SIZE} {...props} />, // Client Item
-    enterprisesIcon: (props) => <LuBuilding2 size={ICON_SIZE} {...props} />,
+    enterprisesIcon: (props) => <LuBuilding size={ICON_SIZE} {...props} />,
 
     // Organization
-    bisIcon: (props) => <LuUsersCog size={ICON_SIZE} {...props} />, // Org Group
+    bisIcon: (props) => <LuBriefcase size={ICON_SIZE} {...props} />, // Org Group
     usersIcon: (props) => <LuUserCog size={ICON_SIZE} {...props} />,
-    employeesIcon: (props) => <LuIdCard size={ICON_SIZE} {...props} />,
+    employeesIcon: (props) => <LuBadge size={ICON_SIZE} {...props} />,
     suppliersIcon: (props) => <LuTruck size={ICON_SIZE} {...props} />,
-    notesIcon: (props) => <LuStickyNote size={ICON_SIZE} {...props} />, // New key: Notas
+    notesIcon: (props) => <LuStickyNote size={ICON_SIZE} {...props} />,
 }
 
 export default navigationIcon
